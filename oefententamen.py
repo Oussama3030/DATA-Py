c129b5c718bc3480ea535941d2b9fb91bc5a6031
Oussama Benchikhi <oussama@MacBook-Air-van-Oussama.local>
2020-01-13 12:15:17 +0100

    jjj

--- oefententamen.py
+++ oefententamen.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-
 raw = []
 header = []
 with open('tentamen.txt','r') as f:
@@ -14,9 +13,37 @@ with open('tentamen.txt','r') as f:
         else:
             header.append(line)
 data = pd.DataFrame(raw,columns = ['','',''])
-print("")
+print("# Oussama Benchikhi 6930263")
 for line in header:
     print(line.strip())
 print(data)
-print("hey")
+
+datamatrix = np.genfromtxt("tentamen.txt")
+gemiddeldey = datamatrix.mean(0)
+
+def pythagoras(lijst):
+    return np.sqrt(np.sum(lijst**2))
+
+v0 = []
+for i in datamatrix:
+    v0.append(pythagoras(i))
+
+print(np.round(gemiddeldey, 2), "de gemiddelde snelheden in m/s")
+print(np.round(v0,2), "de gemiddelde beginsnelheden in m/s")
+
+np.random.seed(20010723)
+delta_v0 = np.random.normal(0, 2, size=np.shape(v0))
+#print(delta_v0)
+#print("delta_v0 =", delta_v0)
+
+V0New = v0+delta_v0
+print(np.round(V0New,2))
+
+
+
+
+
+
+
+
 
